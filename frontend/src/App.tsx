92d00535b6fc6189d40306f30a56c382e1fddc2e
CitrusSoda <dndhkaus1004@naver.com>
2024-06-13 22:58:00 +0900

    🎨 chore: trunk 설정 (linter, prettier ...)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,7 @@ import { Button } from './components/ui/button';
 
 function App() {
   return (
-    <div className="max-w-3xl mx-auto">
+    <div className="mx-auto max-w-3xl">
       <h1>Hello World!</h1>
       <Button>Click me</Button>
     </div>
--- server/app.ts
+++ server/app.ts
@@ -1,6 +1,9 @@
 import { Hono } from 'hono';
+import { logger } from 'hono/logger';
+
 const app = new Hono();
 
+app.use(logger());
 app.get('/api', (c) => c.text('Hello World!'));
 
 export default app;
