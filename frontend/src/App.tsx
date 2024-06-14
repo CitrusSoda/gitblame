acdc5d3169ec8fd6fb3808cedde8288666b019e1
CitrusSoda <dndhkaus1004@naver.com>
2024-06-14 11:23:59 +0900

    ✨ feat: 다크 모드 추가 및 카드 컴포넌트 추가

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,21 @@
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { atomDark } from 'react-syntax-highlighter/dist/esm/styles/prism';
+
+import { CodeQuestionCard } from './components/CodeQuestionCard';
+import ThemeModeToggle from './components/theme/ThemeModeToggle';
 import { Button } from './components/ui/button';
 
 function App() {
+  const codeString = '(num) => num + 1';
   return (
     <div className="mx-auto max-w-3xl">
+      <ThemeModeToggle />
       <h1>Hello World!</h1>
       <Button>Click me</Button>
+      <SyntaxHighlighter language="jsx" style={atomDark}>
+        {codeString}
+      </SyntaxHighlighter>
+      <CodeQuestionCard />
     </div>
   );
 }
---
+++ frontend/src/components/CodeQuestionCard.tsx
@@ -0,0 +1,60 @@
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+import { cn } from '@/lib/utils';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { atomDark } from 'react-syntax-highlighter/dist/esm/styles/prism';
+import { prism } from 'react-syntax-highlighter/dist/esm/styles/prism';
+
+import { useTheme } from './theme/ThemeProvider';
+
+export const CodeQuestionCard = () => {
+  const { theme } = useTheme();
+
+  const codeString = `<Card>
+  <CardHeader>
+    <SyntaxHighlighter language="jsx" style={atomDark}>
+      {codeString}
+    </SyntaxHighlighter>
+  </CardHeader>
+  <CardContent>
+    <p>파이썬 이렇게 쓰는거 맞나요?</p>
+  </CardContent>
+  <CardFooter>
+    <p>이거 해봤는데 이렇게 됐어요</p>
+  </CardFooter>
+</Card>`;
+
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>ShadCn 이렇게 쓰는거 맞나요?</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <SyntaxHighlighter
+          language="jsx"
+          style={theme === 'dark' ? atomDark : prism}
+          className={cn('line-clamp-3')}
+        >
+          {codeString}
+        </SyntaxHighlighter>
+      </CardContent>
+      <CardFooter>
+        <p className="line-clamp-3">
+          이거 해봤는데 이렇게 됐어요 Lorem ipsum dolor, sit amet consectetur
+          adipisicing elit. Voluptates modi repudiandae, cumque ratione quaerat
+          quam? Reiciendis aperiam distinctio commodi laudantium. Ullam
+          doloribus voluptatum illo nostrum cumque! Eligendi soluta ullam
+          itaque. Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+          Laboriosam beatae aperiam ullam illo ad delectus blanditiis explicabo
+          magnam voluptatem ipsa earum eius, tenetur asperiores excepturi!
+          Minima ipsa sit inventore numquam.
+        </p>
+      </CardFooter>
+    </Card>
+  );
+};
